6b1875d6ad4ba266dc96fd3d102a902bb27bc8ac
Vinicius De Antoni <vdeantoni@gmail.com>
2025-10-21 11:12:07 -0700

    fix(ci): add lcov format to test reports

--- packages/blessed/vitest.config.ts
+++ packages/blessed/vitest.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     },
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'json', 'html'],
+      reporter: ['text', 'json', 'html', 'lcov'],
       exclude: [
         'node_modules/**',
         '__tests__/**',
